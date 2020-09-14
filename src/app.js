ce2575b84f0765368d8e26e450168354c56c43f4
Ivan <matteraivam@gmail.com>
2020-09-14 11:35:27 +0200

    seconda section

--- src/app.js
+++ src/app.js
@@ -1,1 +1,43 @@
+var $ = require('jquery');
 
+$(document).ready(function(){
+  $(".prev").click(function(){
+    prevImage();
+  });
+  $(".next").click(function(){
+    nextImage();
+  });
+});
+function nextImage (){
+  var image = $(".images img.active");
+  var first = $(".images img.first");
+  var cerchio = $(".nav i.active");
+  var cerchioF = $(".nav i.first");
+  image.removeClass("active");
+  cerchio.removeClass("active");
+
+  if (image.hasClass("last")) {
+    first.addClass("active");
+    cerchioF.addClass("active");
+  }else{
+    image.next().addClass("active");
+    cerchio.next().addClass("active");
+
+  };
+};
+function prevImage (){
+  var image = $(".images img.active");
+  var last = $(".images img.last");
+  var cerchio = $(".nav i.active");
+  var cerchioF = $(".nav i.last");
+  image.removeClass("active");
+  cerchio.removeClass("active");
+
+  if (image.hasClass("first")) {
+    last.addClass("active");
+    cerchioF.addClass("active");
+  }else{
+    image.prev().addClass("active");
+    cerchio.prev().addClass("active");
+  };
+};
